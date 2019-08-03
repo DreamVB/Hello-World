8ec0cc2f718d8f6713f5da3b9dad85decdc8c3b4
Ben <dreamvb@outlook.com>
2019-08-03 20:14:27 +0100

    Update Version 1.3a

--- bpwsgen.c
+++ bpwsgen.c
@@ -1,15 +1,17 @@
 /*
-    Strong Password Generator
+    Strong Random Password Generator
     by DreamVB
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <time.h>
 #include <string.h>
 
-#define DEFAULT_PWS_LENGTH 8
-#define DEFAULT_PWS_COUNT 1
+#define MIN_PWS_LENGTH 6
+#define MIN_PWS_COUNT 1
+#define MIN_PWS_COUNT 1
 #define MAX_MASK_LEN 256
 #define MAX_PWS_LEN 256
 #define MAX_FLAGS 10
@@ -22,8 +24,9 @@
 char *pLowcase = "abcdefghijklmnopqrstuvwxyz";
 char *pUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char *pDigits = "0123456789";
-char *pSymbol = "!\"#$%&'()*+,-./:;<=>?@[]^_{|}~";
+char *pPunctuation = "!\"#$%&'()*+,-./:;<=>?@[]^_{|}~";
 char *pHex = "ABCDEF0123456789";
+char *sp = " ";
 
 //Holds the option types of passwords to generate.
 int i_flags = 0;
@@ -62,36 +65,42 @@ int is_vowel(char c){
 
 int main(int argc, char **argv)
 {
+    char c = '\0';
     char flag = '\0';
     char pTempMask[MAX_MASK_LEN];
     char pMask[MAX_MASK_LEN];
 
     int pMaskLen = 0;
-    int length = DEFAULT_PWS_LENGTH;
-    int count = DEFAULT_PWS_COUNT;
+    int length = MIN_PWS_LENGTH;
+    int count = MIN_PWS_COUNT;
     int x = 2;
     int i = 0;
     int j = 0;
     int rnd = 0;
-    int rm_vowels = FALSE;
 
+    int rm_vowels = FALSE;
+    int ln_No = FALSE;
+    int Exclude_simlar = FALSE;
     //Get random seed.
     srand((unsigned int)time(NULL));
 
     if(argc < 3){
         printf("-------------------------------------------------------------------------------\n");
-        printf("bpwsgen ::       Strong Password Generator Version 1.3\n");
+        printf("bpwsgen ::  Strong Random Password Generator Version 1.3a\n");
         printf("-------------------------------------------------------------------------------\n");
-        printf("\nSimple Usage :: bpwsgen length count  /[OPTIONS]\n");
+        printf("\nSimple Usage :: bpwsgen length count /[OPTIONS]\n");
         printf("\n    Length ::  Length of the password to create.\n");
         printf("    Count  ::  Number of passwords to create.\n");
         printf("\n    Options ::\n\n");
         printf("       /U Include upper case characters A-Z\n");
         printf("       /L Include lower case characters a-z\n");
         printf("       /D Include digits 0-9\n");
-        printf("       /S Include special characters.\n");
+        printf("       /P Include punctuation and others.\n");
         printf("       /H Include hexadecimal characters.\n");
+        printf("       /S Include space character.\n");
         printf("       /V Do not include vowels.\n");
+        printf("       /X Exclude Similar characters e.g. (Il1O05S2Z)\n");
+        printf("       /N Include number each line.\n");
         return 0;
     }
 
@@ -100,8 +109,8 @@ int main(int argc, char **argv)
     count = atoi(argv[2]);
 
     //Check password length
-    if(length < 4){
-        length = 4;
+    if(length < MIN_PWS_LENGTH){
+        length = MIN_PWS_LENGTH;
     }
     //Max length of password is 255
     if(length > MAX_PWS_LEN){
@@ -109,8 +118,8 @@ int main(int argc, char **argv)
     }
 
     //Check number of passwords.
-    if(count < 1){
-        count = 1;
+    if(count < MIN_PWS_COUNT){
+        count = MIN_PWS_COUNT;
     }
     //Max passwords
     if(count > MAX_PASSWORDS){
@@ -160,16 +169,26 @@ int main(int argc, char **argv)
         case 'D':
             strcat(pTempMask,pDigits);
             break;
-        case 'S':
-            strcat(pTempMask,pSymbol);
+        case 'P':
+            strcat(pTempMask,pPunctuation);
             break;
         case 'H':
             strcat(pTempMask,pHex);
             break;
+        case 'S':
+            strcat(pTempMask,sp);
+            break;
         case 'V':
             rm_vowels = TRUE;
             break;
+        case 'N':
+            ln_No = TRUE;
+            break;
+        case 'X':
+            Exclude_simlar = TRUE;
+            break;
         }
+        //INC counter
         i++;
     }
 
@@ -201,15 +220,35 @@ int main(int argc, char **argv)
     while(i < count){
         //Generate random password.
         for(j = 0; j < length;j++){
-            //Get random number.
-            rnd = rand() % strlen(pMask);
-            //Get random char from pTempMask
-            char c = pMask[rnd];
+
+        //Get random number.
+        rnd = rand() % strlen(pMask);
+            //Get random char from pMask
+            c = pMask[rnd];
+
+            //Exclude similar characters.
+            if(Exclude_simlar == TRUE){
+                while(c == 'I' || c == 'l' || c == '1'
+                      || c == 'O' || c == '0'
+                      || c == '5' || c == 'S'
+                      || c == '2' || c == 'Z'){
+                    //Get random number
+                    rnd = rand() % strlen(pMask);
+                    //Get random char from pMask
+                    c = pMask[rnd];
+                }
+            }
             //Build password
             pws[j] = c;
         }
         //Add ending for password.
         pws[j] = '\0';
+
+        //Check if numbering lines
+        if(ln_No == TRUE){
+            //Print line number.
+            printf("%04d ",(i + 1));
+        }
         //Print random password.
         printf("%s\n",pws);
         //INC counter
